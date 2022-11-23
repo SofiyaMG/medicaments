892ba44449d80d82daaa66639f4932df0c38a622
SofiyaMG <Smushyandi@gmail.com>
2022-11-23 16:40:20 +0100

    improved mevu for interface

--- interface.sh
+++ interface.sh
@@ -2,4 +2,8 @@
 
 echo "MEDICAMENTS"
 
-read -p "rechercher un medicament comme? nom/patho/sub: "
+read -p "rechercher un medicament comme? nom/patho/sub: " choix
+
+if [ $choix = "nom" ]
+then
+	read -p "Quel nom?" name
