fb4ad089c4ae3b027c1e738c080f3efb39e023b4
SofiyaMG <Smushyandi@gmail.com>
2022-11-23 16:53:22 +0100

    added patho and sub menu

--- interface.sh
+++ interface.sh
@@ -6,4 +6,11 @@ read -p "rechercher un medicament comme? nom/patho/sub: " choix
 
 if [ $choix = "nom" ]
 then
-	read -p "Quel nom?" name
+	read -p "Quel nom? " name
+elif [ $choix = "patho" ]
+then
+	read -p "Quel patho? " patho
+elif [ $choix = "sub" ]
+then
+	read -p "Quelle sub? " sub
+fi
